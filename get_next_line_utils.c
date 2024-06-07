f3aac3234b20339b61e025e3df50db751c4fb5d1
Tony <160587075+42Monkey@users.noreply.github.com>
2024-06-07 19:22:40 +0700

    final release, 100%

---
+++ get_next_line_utils.c
@@ -0,0 +1,55 @@
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*s3;
+
+	if (!s1 && !s2)
+		return (NULL);
+	s3 = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!s3)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1 && s1[i])
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	while (s2 && s2[j])
+	{
+		s3[i + j] = s2[j];
+		j++;
+	}
+	s3[i + j] = '\0';
+	if (s1)
+		free(s1);
+	return (s3);
+}
